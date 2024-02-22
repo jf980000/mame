56439846d368ed888831c7de4de0a94225e3ebf9
987123879113 <63495610+987123879113@users.noreply.github.com>
2024-02-23 01:21:20 +0900

    psx/dma: HACK: Add small timer to certain DMA ch 4 writes to fix Dance Maniax screeching noise

--- src/devices/cpu/psx/dma.cpp
+++ src/devices/cpu/psx/dma.cpp
@@ -276,7 +276,15 @@ void psxdma_device::write(offs_t offset, uint32_t data, uint32_t mem_mask)
 				{
 					LOG( "%s: dma %d write block %08x %08x\n", machine().describe_context(), index, n_address, n_size );
 					dma->fn_write( m_ram, n_address, n_size );
-					dma_finished( index );
+
+					if ( index == 4 )
+					{
+						dma_start_timer( index, 64 ); // Hack for Dance Maniax
+					}
+					else
+					{
+						dma_finished( index );
+					}
 				}
 				else if( dma->n_channelcontrol == 0x11050100 &&
 					!dma->fn_write.isnull() )
